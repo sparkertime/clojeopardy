72ec86e341e4a8bdfae5359db83b3262331905f4
Scott Parker <scott.p.parker@gmail.com>
2012-02-06 20:01:15 -0600

    Modified README, project.clj with more detail

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject clojeopardy "1.0.0-SNAPSHOT"
-  :description "Clojeopardy: A J! Archive Scraper built with Enlive"
+(defproject clojeopardy "0.5.0-SNAPSHOT"
+  :description "Clojeopardy: A J! Archive scraper built with Enlive"
   :url "http://github.com/citizenparker/clojeopardy"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [enlive "1.0.0-SNAPSHOT"]
