3a55d136fc39cf7ba2f6e7fbbf4222da80f126ff
Scott Parker <scott.p.parker@gmail.com>
2012-04-13 08:50:49 -0500

    Added docstrings and made most methods private, since #clues and #categories should really be the entry points of this library. Still some cleaning up to be done though

--- src/clojeopardy/core.clj
+++ src/clojeopardy/core.clj
@@ -1,52 +1,70 @@
 (ns clojeopardy.core
   (:require [net.cgrand.enlive-html :as html]))
 
-(def base-url "http://www.j-archive.com/showgame.php?game_id=")
+(defn build-url
+  "Given a game id, returns the proper URL to retrieve it"
+  [id]
+  (java.net.URL. (str "http://www.j-archive.com/showgame.php?game_id=" id)))
 
-(defn build-url [id]
-  (java.net.URL. (str base-url id)))
-
-(defn fetch-game-live [id]
+(defn fetch-game-live
+  "Given a game id, returns all HTML in that game's page"
+  [id]
   (html/html-resource (build-url id)))
 
 (def fetch-game (memoize fetch-game-live))
+
+(defn- name-for
+  "Returns the category name of a particular category element"
+  [element]
+  (html/text (first (html/select element [:.category_name]))))
+
+(defn- comments-for
+  "Returns any of Alex's comments for a particular category element"
+  [element]
+  (html/text (first (html/select element [:.category_comments]))))
+
+(defn- parse-category
+  "Transforms a category element in position idx in a round into a map containing :name, :comments, :round, and :pos"
+  [element idx round]
+  { :name (name-for element)
+    :comments (comments-for element)
+    :round round
+    :pos (inc idx) })
 
 (def css-class-for-round
   {:single :#jeopardy_round
    :double :#double_jeopardy_round
    :final :#final_jeopardy_round})
 
-(defn name-for [element]
-  (html/text (first (html/select element [:.category_name]))))
+(defn- categories-for-round
+  "For a particular round and game id, returns all categories within the round"
+  [round id]
+  (map-indexed #(parse-category %2 %1 round)  (html/select (fetch-game id) [(css-class-for-round round) :.category])))
 
-(defn comments-for[element]
-  (html/text (first (html/select element [:.category_comments]))))
-
-(defn parse-category [idx element round]
-  { :name (name-for element)
-    :comments (comments-for element)
-    :round round
-    :pos (inc idx) })
-
-(defn categories-for-round [round id]
-  (map-indexed #(parse-category %1 %2 round)  (html/select (fetch-game id) [(css-class-for-round round) :.category])))
-
-(defn categories [id]
+(defn categories
+  "Given a particular game, returns all categories in that game as a collection of maps"
+  [id]
   (flatten
     (for [round [:single :double :final]]
       (categories-for-round round id))))
 
-(defn question-for [element]
+(defn- question-for
+  "Returns the correct question for a particular clue element"
+  [element]
   (re-find #"(?<=correct_response\">).*(?=</em>)"
            ((element :attrs) :onmouseover)))
 
-(defn answer-for [element]
+(defn- answer-for
+  "Returns the answer for a particular clue element"
+  [element]
   (re-find #"(?<=_stuck', ').*(?='\))"
            ((element :attrs) :onmouseout)))
 
 (def round-for-shorthand {"J" :single "DJ" :double "FJ" :final})
 
-(defn category-for [element game-id]
+(defn- category-for
+  "Returns the category map for the clue element in a particular game."
+  [element game-id]
   (let [round-str (re-find #"(?<= 'clue_).*(?=_stuck)"
                          ((element :attrs) :onmouseout))]
     (if (= "FJ" round-str)
@@ -56,20 +74,28 @@
             order (Integer. (re-find #"(?<=J_)[0-9]*" round-str))]
         (nth (categories-for-round round game-id) (dec order))))))
 
-(defn pos-for [element]
-  (if-let [pos-str (re-find #"(?<=J_[0-9]_).*(?=_stuck)" 
+(defn- pos-for
+  "Returns a clue element's position in the sequence of a category's clues. Final Jeopardy clues will have no position."
+  [element]
+  (if-let [pos-str (re-find #"(?<=J_[0-9]_).*(?=_stuck)"
                      ((element :attrs) :onclick))]
     (Integer. pos-str)
     0))
 
-(defn id-for [element]
+(defn- id-for
+  "Returns the id of a clue element. Final Jeopardy clues will have no id"
+  [element]
   (when-let [link (first (html/select element [:.clue_order_number :a]))]
        (re-find #"(?<=clue_id=).*" ((link :attrs) :href))))
 
-(defn daily-double? [element]
+(defn- daily-double?
+  "Returns true or false to indicate whether a clue element is a daily double"
+  [element]
   (boolean (seq (html/select element [:.clue_value_daily_double]))))
 
-(defn parse-clue [element game-id]
+(defn- parse-clue
+  "Given a div element containing a clue and a game-id, transforms that element into a map containing :question, :answer, :pos, :id, :daily-double?, and :category (this last element itself a map - see parse-category). Note that :id will not be available for Final Jeopardy clues"
+  [element game-id]
   { :question (question-for element)
     :answer (answer-for element)
     :category (category-for element game-id)
@@ -77,7 +103,9 @@
     :id (id-for element)
     :daily-double? (daily-double? element)})
 
-(defn clues [id]
+(defn clues
+  "Returns all answer/question pairs in a particular game as a collection of maps"
+  [id]
   (map #(parse-clue %1 id) (concat
                              (html/select (fetch-game id) [:.clue :div])
                              (html/select (fetch-game id) [:.final_round :div]))))
